1b644e530602c5ffbc281ea1be5b15517eaf19e5
haykuro <haykuro@gmail.com>
2024-10-07 16:07:16 -0400

    dynamic screen rendering

--- pyramid.c
+++ pyramid.c
@@ -2,9 +2,9 @@
 #include <math.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 
-#define SCREEN_WIDTH 80
-#define SCREEN_HEIGHT 22
 #define FOV 30  // Increase the field of view slightly
 #define ZOOM 100  // Adjusted zoom for better projection
 
@@ -22,13 +22,22 @@ int edges[8][2] = {
     {1, 2}, {2, 3}, {3, 4}, {4, 1}  // Base edges
 };
 
+// Function to get the terminal width and height
+void get_terminal_size(int *width, int *height) {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);  // Get the terminal size
+    *width = w.ws_col;
+    *height = w.ws_row;
+}
+
+// Function to rotate a point
 void rotate(float x, float y, float z, float* new_x, float* new_y, float* new_z, float angleX, float angleY, float angleZ) {
     float cosa = cos(angleX), sina = sin(angleX);
     float cosb = cos(angleY), sinb = sin(angleY);
     float cosc = cos(angleZ), sinc = sin(angleZ);
-
+    
     float newx = x, newy = y, newz = z;
-
+    
     float temp_y = cosa * newy - sina * newz;
     newz = sina * newy + cosa * newz;
     newy = temp_y;
@@ -46,15 +55,17 @@ void rotate(float x, float y, float z, float* new_x, float* new_y, float* new_z,
     *new_z = newz;
 }
 
-void project(float x, float y, float z, int* screen_x, int* screen_y) {
-    *screen_x = (int)(SCREEN_WIDTH / 2 + ZOOM * x / (z + FOV));
-    *screen_y = (int)(SCREEN_HEIGHT / 2 - ZOOM * y / (z + FOV));
+// Project 3D points onto 2D screen
+void project(float x, float y, float z, int* screen_x, int* screen_y, int screen_width, int screen_height) {
+    *screen_x = (int)(screen_width / 2 + ZOOM * x / (z + FOV));
+    *screen_y = (int)(screen_height / 2 - ZOOM * y / (z + FOV));
 }
 
-void render_frame(float angleX, float angleY, float angleZ) {
-    char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
-    for (int i = 0; i < SCREEN_HEIGHT; ++i)
-        for (int j = 0; j < SCREEN_WIDTH; ++j)
+// Main rendering function
+void render_frame(float angleX, float angleY, float angleZ, int screen_width, int screen_height) {
+    char screen[screen_height][screen_width];
+    for (int i = 0; i < screen_height; ++i)
+        for (int j = 0; j < screen_width; ++j)
             screen[i][j] = ' ';
 
     for (int i = 0; i < 8; ++i) {
@@ -73,15 +84,15 @@ void render_frame(float angleX, float angleY, float angleZ) {
 
         int screen_x1, screen_y1, screen_x2, screen_y2;
 
-        project(new_x1, new_y1, new_z1, &screen_x1, &screen_y1);
-        project(new_x2, new_y2, new_z2, &screen_x2, &screen_y2);
+        project(new_x1, new_y1, new_z1, &screen_x1, &screen_y1, screen_width, screen_height);
+        project(new_x2, new_y2, new_z2, &screen_x2, &screen_y2, screen_width, screen_height);
 
         int dx = abs(screen_x2 - screen_x1), sx = screen_x1 < screen_x2 ? 1 : -1;
-        int dy = -abs(screen_y2 - screen_y1), sy = screen_y1 < screen_y2 ? 1 : -1;
+        int dy = -abs(screen_y2 - screen_y1), sy = screen_y1 < screen_y2 ? 1 : -1; 
         int err = dx + dy, e2;
 
         while (1) {
-            if (screen_x1 >= 0 && screen_x1 < SCREEN_WIDTH && screen_y1 >= 0 && screen_y1 < SCREEN_HEIGHT)
+            if (screen_x1 >= 0 && screen_x1 < screen_width && screen_y1 >= 0 && screen_y1 < screen_height)
                 screen[screen_y1][screen_x1] = '#';
             if (screen_x1 == screen_x2 && screen_y1 == screen_y2) break;
             e2 = 2 * err;
@@ -90,8 +101,8 @@ void render_frame(float angleX, float angleY, float angleZ) {
         }
     }
 
-    for (int i = 0; i < SCREEN_HEIGHT; ++i) {
-        for (int j = 0; j < SCREEN_WIDTH; ++j)
+    for (int i = 0; i < screen_height; ++i) {
+        for (int j = 0; j < screen_width; ++j)
             putchar(screen[i][j]);
         putchar('\n');
     }
@@ -99,14 +110,16 @@ void render_frame(float angleX, float angleY, float angleZ) {
 
 int main() {
     float angleX = 0, angleY = 0, angleZ = 0;
+    int screen_width, screen_height;
 
     while (1) {
-        render_frame(angleX, angleY, angleZ);
+        get_terminal_size(&screen_width, &screen_height);  // Get the current terminal size
+        render_frame(angleX, angleY, angleZ, screen_width, screen_height);
         angleX += 0.04;
         angleY += 0.05;
         angleZ += 0.03;
-        usleep(50000);
-        printf("\033[H\033[J");
+        usleep(50000);  // Slow down the rotation for better visualization
+        printf("\033[H\033[J");  // Clear the screen for animation
     }
 
     return 0;
